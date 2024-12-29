e967f531521bc325a48301f910a8fb8db4dd4f6d
Ariel Aharon <xarielah@gmail.com>
2024-12-29 13:07:24 +0200

    feat: new login and signup pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router"
+import { userService } from "../services/user.service"
+
+const Login = () => {
+    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+    const [error, setError] = useState(null)
+
+    const loggedInUser = useSelector(state => state.userModule.user)
+    if (loggedInUser)
+        return <Navigate to="/home" />
+
+    const handleChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        userService.login(credentials)
+            .then(user => {
+                console.log("🚀 ~ handleSubmit ~ user:", user)
+                setError(null)
+                setCredentials(userService.getEmptyCredentials())
+            })
+            .catch(err => {
+                setError(err)
+            })
+    }
+
+    return (
+        <form className="login centered-container" onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            {error && <p className="auth-error">{error}</p>}
+            <input className="input" onChange={handleChange} name="username" type="text" placeholder="Enter your username" />
+            <input className="input" onChange={handleChange} name="password" type="password" placeholder="Enter your password" />
+            <button type="submit">Login</button>
+        </form>
+    )
+}
+
+export default Login;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router"
+import { userService } from "../services/user.service"
+
+const SignUp = () => {
+    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+    const [error, setError] = useState(null)
+
+    const loggedInUser = useSelector(state => state.userModule.user)
+    if (loggedInUser)
+        return <Navigate to="/home" />
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!credentials.username || !credentials.password || !credentials.fullname)
+            return setError("Please fill all the required fields")
+        userService.signup(credentials)
+            .then(user => {
+                console.log("🚀 ~ handleSubmit ~ user:", user)
+                setError(null)
+                setCredentials(userService.getEmptyCredentials())
+            })
+            .catch(err => {
+                setError(err)
+            })
+    }
+
+    const handleChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <form className="signup centered-container" onSubmit={handleSubmit}>
+            <h1>SignUp</h1>
+            {error && <p className="auth-error">{error}</p>}
+            <input className="input" onChange={handleChange} name="username" type="text" placeholder="Enter your username" required />
+            <input className="input" onChange={handleChange} name="fullname" type="text" placeholder="Enter your fullname" required />
+            <input className="input" onChange={handleChange} name="password" type="password" placeholder="Enter your password" required />
+            <button type="submit">Sign Up</button>
+        </form>
+    )
+}
+
+export default SignUp;
